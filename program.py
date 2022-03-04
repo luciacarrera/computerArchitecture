b2a903d6abe0f25c83a9c0690c82347e0d028013
Lucia Carrera <luciaacarreraa@gmail.com>
2022-03-03 19:18:03 -0500

    restructuring

--- program.py
+++ program.py
@@ -3,10 +3,7 @@ import math
 from Cache import Cache
 from Memory import Memory
 
-'''#from torch import le
-# global variable for memory (Bytearray)
-block = [[],[]]
-blocks = []
+'''
 
 ### MAIN FUNCTION
 def main():
@@ -59,63 +56,79 @@ class Simulator:
         tag_bits = self.address_bits - index_bits - offset_bits
 
         return tag_bits, index_bits, offset_bits
-
-'''
-### FUNCTION to calculate address mapping, address in integer format
-def mapping(cache, address):    
-    # get bits from cache
-    length = cache.memory_address
-    tag_bits, index_bits, offset_bits = cache.bits()
-
-    # transform to binary
-    binary = format(address, "b")
-    # add additional zeroes to string
-    binary= binary.zfill(length)
-
-    # get block offset and transform binary to integer
-    end = length
-    start = length - offset_bits
-    offset = int(binary[start:end],2)
-
-    # get index
-    end = start
-    start = end - index_bits
-    index = int(binary[start:end],2)
-
-    # get tag
-    end = tag_bits
-    start = 0
-    tag = int(binary[start:end],2)
-
-    range0 = int(format(tag, "b") + format(index, "b") + "000000",2)
-    range1 = int(format(tag, "b") + format(index, "b") + "111111",2)
-    print("Address:",address,"Binary:",binary, "Tag:", tag, "; Index:",index, "; Offest:",offset, "[", range0, "-", range1, "]")
-    return tag, index, offset
-
-
-### FUNCTION that returns the value read from the specified address an
-def read_word(my_cache, address):
-    # check each address for four-bit alignment
-    # check for range 0 ≤ address < memSize, where memSize is the number of bytes in the memory.
-    tag, index, offset = mapping(my_cache, address)
-    #word = blocks[index][offset]
-    #process_word()
-    #return word, tag
-
-
-### FUNCTION that writes the provided word to the specified address.
-def write_word(address, word):
-    # check each address for four-bit alignment
-    # check for range 0 ≤ address < memSize, where memSize is the number of bytes in the memory.
-    word = ""
-    process_word()
-
-
-### FUNCTION that processes read and write as a single underlying function
-def process_word():
-    print()
-
     
-### TESTING
-main()
-'''
+    ### FUNCTION that returns the value read from the specified address an
+    def read_word(self, word):
+        # first map address 
+        address = mapping(self, word)
+        # check each address for four-bit alignment
+        assert self.is_aligned(word) == False, 'Error: this address is not aligned in a four byte boundary'
+        # check for range 0 ≤ address < memSize, where memSize is the number of bytes in the memory.
+        pass
+    
+    # to check if word aligned in a four bit boundary
+    def is_aligned(self, address):
+        if (address & 0x3) == 0 :
+            return True
+        else:
+            return False
+
+
+    '''    ### FUNCTION to calculate address mapping, address in integer format
+    def mapping(cache, address):    
+        # get bits from cache
+        length = cache.memory_address
+        tag_bits, index_bits, offset_bits = cache.bits()
+
+        # transform to binary
+        binary = format(address, "b")
+        # add additional zeroes to string
+        binary= binary.zfill(length)
+
+        # get block offset and transform binary to integer
+        end = length
+        start = length - offset_bits
+        offset = int(binary[start:end],2)
+
+        # get index
+        end = start
+        start = end - index_bits
+        index = int(binary[start:end],2)
+
+        # get tag
+        end = tag_bits
+        start = 0
+        tag = int(binary[start:end],2)
+
+        range0 = int(format(tag, "b") + format(index, "b") + "000000",2)
+        range1 = int(format(tag, "b") + format(index, "b") + "111111",2)
+        print("Address:",address,"Binary:",binary, "Tag:", tag, "; Index:",index, "; Offest:",offset, "[", range0, "-", range1, "]")
+        return tag, index, offset
+
+
+    ### FUNCTION that returns the value read from the specified address an
+    def read_word(my_cache, address):
+        # check each address for four-bit alignment
+        # check for range 0 ≤ address < memSize, where memSize is the number of bytes in the memory.
+        tag, index, offset = mapping(my_cache, address)
+        #word = blocks[index][offset]
+        #process_word()
+        #return word, tag
+
+
+    ### FUNCTION that writes the provided word to the specified address.
+    def write_word(address, word):
+        # check each address for four-bit alignment
+        # check for range 0 ≤ address < memSize, where memSize is the number of bytes in the memory.
+        word = ""
+        process_word()
+
+
+    ### FUNCTION that processes read and write as a single underlying function
+    def process_word():
+        print()
+
+        
+    ### TESTING
+    main()
+    '''
